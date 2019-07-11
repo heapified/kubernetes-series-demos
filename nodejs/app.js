9565fe9a8d61f97ea5a5e7c244f8deb5ad400fc8
heapified <maxheapified@gmail.com>
2019-07-10 23:37:30 -0700

    Services and ingress

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,4 +5,12 @@ var number = Math.random()
 
 app.get('/', (req, res) => res.send(`Hello, my number is ${number}`))
 
+
+app.get('/foo', (req, res) => res.send(`FOO, my number is ${number}`))
+app.get('/foobar', (req, res) => res.send(`FOOBAR, my number is ${number}`))
+app.get('/bar', (req, res) => res.send(`BAR, my number is ${number}`))
+
+
+app.get('/anotherpath', (req, res) => res.send(`ANOTHERPATH, my number is ${number}`))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
